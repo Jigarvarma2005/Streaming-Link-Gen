31a390a99fe073750ad40b9739d194405002acfc
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-09-22 09:33:23 +0530

    Make config more better

--- config.py
+++ config.py
@@ -16,8 +16,16 @@ class Config(object):
     # Google drive folder id
     parent_id = os.environ.get("GDRIVE_FOLDER_ID", "")
     # Set True is drive folder id is Team Drive
-    IS_TEAM_DRIVE = os.environ.get("IS_TEAM_DRIVE", True)
+    IS_TEAM_DRIVE = os.environ.get("IS_TEAM_DRIVE", "False")
+    if IS_TEAM_DRIVE.lower() == 'true':
+        IS_TEAM_DRIVE = True
+    else:
+        IS_TEAM_DRIVE = False
     # Set it True if using Service Account for uploading
-    USE_SERVICE_ACCOUNTS = os.environ.get("USE_SERVICE_ACCOUNTS", False)
+    USE_SERVICE_ACCOUNTS = os.environ.get("USE_SERVICE_ACCOUNTS", "False")
+    if USE_SERVICE_ACCOUNTS.lower() == 'true':
+        USE_SERVICE_ACCOUNTS = True
+    else:
+        USE_SERVICE_ACCOUNTS = False
     # Your gdrive index url (Important)
     INDEX_URL = os.environ.get("INDEX_URL", "")
